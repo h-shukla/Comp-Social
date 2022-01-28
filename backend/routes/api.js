14c1a7beda54b98b45e29413140a920a8cc4cd1d
Himanshu Shukla <hshukla2611@gmail.com>
2022-01-28 22:29:09 +0530

    complete createuser and getuser api

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -3,9 +3,10 @@ const router = express.Router();
 const User = require('../models/User');
 const { body, validationResult } = require('express-validator');
 
+// ROUTE 1: To get user already existing in the database
 router.get('/getuser', async (req, res)=>{
     try {
-        const userID = req.user.id;
+        const userID = req.body.id;
         const user = await User.findById(userID);
         return res.send(user);
     } catch (e) {
@@ -14,8 +15,10 @@ router.get('/getuser', async (req, res)=>{
     }
 });
 
+// ROUTE 2: To create a new user
+// TODO: getting stuck in user with this email exists
 router.post('/createuser', [
-//    body("name", "Enter a valid name").isLength({min:3}),
+    body("name", "Enter a valid name").isLength({min:3}),
     body("email", "Enter a valid email").isEmail(),
     body("password", "Enter password more than 6 characters").isLength({min:6})
 ], async (req, res) => {
@@ -29,24 +32,25 @@ router.post('/createuser', [
     }
     try {
         // Check if the user already exists
-        let user = User.findOne({
+        let user = await User.findOne({
             email: req.body.email
         });
-        if (user) {
+        console.log(req.body.email);
+        if (user!==null) {
             console.log(user);
             return res.status(400).json({
                 success: false,
                 error: "Sorry, a user with this email already exists"
             });
+        } else {
+            // if user doesn't exist create the user
+            user = await User.create({
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+                profiles: req.body.profiles
+            });
         }
-        // if user doesn't exist create the user
-        user = await User.create({
-            name: req.body.name,
-            email: req.body.email,
-            password: req.body.password,
-            profiles: req.body.profiles
-        });
-
         const uniqueUserId = user.id;
         console.log("User created: \n" + user);
         return res.json({
