fdd93706fff4b04ecad04380595682eafd6cf5ee
Himanshu Shukla <hshukla2611@gmail.com>
2022-02-04 01:17:43 +0530

    start navbar design

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../App.css';
+
+function Navbar() {
+  return (
+    <>
+      <nav>
+          <h1>Comp Social</h1>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact us</a></li>
+        </ul>
+      </nav>
+    </>
+  )
+};
+
+export default Navbar;
