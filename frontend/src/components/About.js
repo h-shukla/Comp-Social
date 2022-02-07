2b1befe9ec3cbca3acafb8ee99b19bf42b9af4df
Himanshu Shukla <hshukla2611@gmail.com>
2022-02-07 19:29:35 +0530

    Login page design complete

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
 function About() {
-  return <div>
-      <h1>This is About page</h1>
-  </div>;
+    return(
+        <div>
+          <h1>This is About page</h1>
+        </div>
+    );
 }
 
 export default About;
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 import '../App.css';
 
 function Navbar() {
-  return (
-    <>
-      <nav>
-        <h1 className="main-logo">Comp Social</h1>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/login">Login / Sign up</Link></li>
-        </ul>
-      </nav>
-    </>
-  );
+    return (
+        <>
+          <nav>
+            <h1 className="main-logo">Comp Social</h1>
+            <ul>
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/about">About</Link></li>
+              <li><Link to="/login">Login / Sign up</Link></li>
+            </ul>
+          </nav>
+        </>
+    );
 };
 
 export default Navbar;
